2003b60364c34377acc9f9ed2cba2d1a16037d46
hly99 <1055503066@qq.com>
2019-04-26 14:44:22 +0800

    Update Button.vue

--- src/views/component/Button.vue
+++ src/views/component/Button.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="main">
+<el-row>
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+</el-row>
+
+<el-row>
+  <el-button type="primary" size="medium">大</el-button>
+  <el-button type="primary" size="small">中</el-button>
+  <el-button type="primary" size="mini">小</el-button>
+
+</el-row>
+
+<el-row>
+  <el-button type="text">按键</el-button>
+
+</el-row>
+<el-row>
+  <el-button disabled>默认按钮</el-button>
+  <el-button type="primary" disabled>主要按钮</el-button>
+  <el-button type="success" disabled>成功按钮</el-button>
+  <el-button type="info" disabled>信息按钮</el-button>
+  <el-button type="warning" disabled>警告按钮</el-button>
+  <el-button type="danger" disabled>危险按钮</el-button>
+</el-row>
   </div>
 </template>
 
