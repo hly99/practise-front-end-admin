0bdef7a8e1348a9be7f0853440ea8f7f5e3177c7
hly99 <1055503066@qq.com>
2019-04-26 19:01:58 +0800

    Update Message-box.vue

--- src/views/component/Message-box.vue
+++ src/views/component/Message-box.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="main">
-    <el-button type="danger">删除</el-button>
+    
+    <el-button type="danger" @click="open1">删除</el-button>
     <br>
     姓名: {{name}}
-    <el-button :plain="true">录入姓名</el-button>
+    <el-button :plain="true" @click="open2">录入姓名</el-button>
 
   </div>
 </template>
@@ -16,9 +17,25 @@ export default {
     }  
   },
   methods: {
-    
+    open1(){
+      this.$alert('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确认删除',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        })
+    },
+    open2(){
+    this.$prompt(`请输入姓名`,'提示').then(({ value }) =>{
+      this.name=value
+    })
+  }
   }
 }
+alert("本网站基于 Vue 开发");
 </script>
 
 <style scoped></style>
