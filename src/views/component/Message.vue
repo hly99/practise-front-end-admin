0b9ca319765f36d7dcbf386d95c00426523901c8
hly99 <1055503066@qq.com>
2019-04-26 16:14:06 +0800

    Update Message.vue

--- src/views/component/Message.vue
+++ src/views/component/Message.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main">
-    <el-button :plain="true">成功</el-button>
-    <el-button :plain="true">警告</el-button>
-    <el-button :plain="true">消息</el-button>
-    <el-button :plain="true">错误</el-button>
+    <el-button :plain="true" @click="open1">成功</el-button>
+    <el-button :plain="true" @click="open2">警告</el-button>
+    <el-button :plain="true" @click="open3">消息</el-button>
+    <el-button :plain="true" @click="open4">错误</el-button>
   </div>
 </template>
 
@@ -15,8 +15,20 @@ export default {
     }
   },
   methods: {
-    
+    open1(){
+      this.$message.success( '这是一条成功的信息' );
+    },
+    open2(){
+    this.$message.warning('这是一条警告的信息')
+  },
+    open3(){
+      this.$message.info('这是一条信息')
+    },
+    open4(){
+      this.$message.error('这是一条错误信息')
+    }
   }
+  
 }
 </script>
 
