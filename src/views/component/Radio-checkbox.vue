0f36d43cf741fd375b8d634e81531a8917b1ac02
hly99 <1055503066@qq.com>
2019-04-27 17:18:27 +0800

    Update Radio-checkbox.vue

--- src/views/component/Radio-checkbox.vue
+++ src/views/component/Radio-checkbox.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="main">
+    <el-checkbox v-model="checked">单选</el-checkbox>
+    <el-checkbox-group v-model="checkList">
+    <el-checkbox label="多选1"></el-checkbox>
+    <el-checkbox label="多选2"></el-checkbox>
+    <el-checkbox label="多选3"></el-checkbox>
+  </el-checkbox-group>
   </div>
 </template>
 
@@ -7,6 +13,8 @@
 export default {
   data() {
     return {
+      checked:false,
+      checkList:['多选1']
 
     }  
   },
