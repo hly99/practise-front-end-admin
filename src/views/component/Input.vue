3c88e07dd385ee31005d9bdeac2f0bd4a34c8ad9
hly99 <1055503066@qq.com>
2019-04-28 14:03:11 +0800

    Update Input.vue

--- src/views/component/Input.vue
+++ src/views/component/Input.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="main">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input
+  type="textarea"
+  :rows="3"
+  placeholder="请输入内容"
+  v-model="textarea">
+</el-input>
+<el-input v-model="input2" type="number" placeholder="请输入数字"></el-input>
   </div>
 </template>
 
@@ -7,7 +15,9 @@
 export default {
   data() {
     return {
-
+      input:'',
+      textarea:'',
+      input2:''
     }  
   },
   methods: {
