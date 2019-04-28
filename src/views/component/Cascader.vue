12f3fe7bfbeb0ba98612ab973015e45860979b36
hly99 <1055503066@qq.com>
2019-04-28 18:40:15 +0800

    Update Cascader.vue

--- src/views/component/Cascader.vue
+++ src/views/component/Cascader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="main">
     选择啤酒: 
+    <el-cascader :options="mainType" v-model="pi" :props="props"></el-cascader>
+    <el-cascader :options="opo" :props="props"></el-cascader>
   </div>
 </template>
 
@@ -42,11 +44,28 @@ export default {
         id: 'lsf10',
         parentId:  'tappist',
         name: '罗斯福10号'
-      }]
+      }],
+      pi:[],
+      props: {
+          value: 'id',
+          label: 'name'
+        },
+      app:[]
     }
   },
-  methods: {
-    
+  computed: {
+    opo:function(){
+      this.app=[]
+      if(this.pi.length==0){
+        return 
+      }
+      for(let i of this.subType){
+        if(this.pi.join("")===i.parentId){
+          this.app.push(i)
+        }
+      }
+      return this.app
+    }
   }
 }
 </script>
